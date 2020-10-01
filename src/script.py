ccf2ff156ca49d5c3f94b9866809bf631a21e5f5
František Gič <mcfreddie777@gmail.com>
2020-10-01 15:55:29 +0200

    Save hashtags into hashmap

--- src/script.py
+++ src/script.py
@@ -44,61 +44,65 @@ def save_tweet(obj):
     Saves each tweet to the database
     :param obj:
     """
-    tweet = Tweet(
-        id=obj['id_str'],
-        content=obj['full_text'],
-        location=None,  # Todo
-        retweet_count=obj['retweet_count'],
-        favorite_count=obj['favorite_count'],
-        happened_at=obj['created_at']
-    )
+    key = obj['id_str']
+    if not key in tweets_map:
 
-    # if user is present in tweet
-    if obj['user'] is not None:
+        tweet = Tweet(
+            id=obj['id_str'],
+            content=obj['full_text'],
+            location=None,  # Todo
+            retweet_count=obj['retweet_count'],
+            favorite_count=obj['favorite_count'],
+            happened_at=obj['created_at']
+        )
+        tweets_map[key] = True
 
-        # if user is not previously added in hashmap of accounts create new user
-        key = obj['user']['id']
-        if not key in accounts_map:
-            account = Account(
-                id=obj['user']['id'],
-                screen_name=obj['user']['screen_name'],
-                name=obj['user']['name'],
-                description=obj['user']['description'],
-                followers_count=obj['user']['followers_count'],
-                friends_count=obj['user']['friends_count'],
-                statuses_count=obj['user']['statuses_count']
-            )
-            accounts_map[account.id] = True
-        else:
-            # find user in database
-            account = session.query(Account).filter(Account.id == key).scalar()
+        # if user is present in tweet
+        if obj['user'] is not None:
 
-        # add user as an author of the tweet
-        tweet.author = account
+            # if user is not previously added in hashmap of accounts create new user
+            key = obj['user']['id']
+            if not key in accounts_map:
+                account = Account(
+                    id=obj['user']['id'],
+                    screen_name=obj['user']['screen_name'],
+                    name=obj['user']['name'],
+                    description=obj['user']['description'],
+                    followers_count=obj['user']['followers_count'],
+                    friends_count=obj['user']['friends_count'],
+                    statuses_count=obj['user']['statuses_count']
+                )
+                accounts_map[account.id] = True
+            else:
+                # find user in database
+                account = session.query(Account).filter(Account.id == key).scalar()
 
-    # if place is present in tweet and has all fields present
-    if (
-            obj['place'] is not None and
-            obj['place']['country_code'] and
-            obj['place']['country_code']
-    ):
-        # if place is not previously added in hashmap of countries create a new country
-        key = obj['place']['country_code']
-        if not key in countries_map:
-            country = Country(
-                code=obj['place']['country_code'],
-                name=obj['place']['country']
-            )
-            countries_map[country.code] = True
-        else:
-            # find country in database
-            country = session.query(Country).filter(Country.code == key).scalar()
+            # add user as an author of the tweet
+            tweet.author = account
 
-        # add place as an country of the tweet
-        tweet.country = country
+        # if place is present in tweet and has all fields present
+        if (
+                obj['place'] is not None and
+                obj['place']['country_code'] and
+                obj['place']['country_code']
+        ):
+            # if place is not previously added in hashmap of countries create a new country
+            key = obj['place']['country_code']
+            if not key in countries_map:
+                country = Country(
+                    code=obj['place']['country_code'],
+                    name=obj['place']['country']
+                )
+                countries_map[country.code] = True
+            else:
+                # find country in database
+                country = session.query(Country).filter(Country.code == key).scalar()
 
-    # save tweet object into the db
-    session.add(tweet)
+            # add place as an country of the tweet
+            tweet.country = country
+
+        # save tweet object into the db
+        session.add(tweet)
 
 
 # init session
@@ -118,7 +122,7 @@ if (DEBUG):
     print('len(accounts_map)', len(accounts_map))
     print('len(countries_map)', len(countries_map))
     print('len(tweets_map)', len(tweets_map))
-    print('len(accounts_map)', len(accounts_map))
+    print('len(hashtags_map)', len(hashtags_map))
 else:
     files = get_input_files()
     parse_each_file(files, save_tweet)
